7cedf0e68786de5069a88d38a3c7778dd983e64a
Marcus Gabriel Dantas Salgues <marcusgabriel.ds@gmail.com>
2018-08-31 13:41:43 -0300

    FT now checks we can input a to-do intem

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,8 @@
 import unittest
+import time
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -19,19 +21,34 @@ class NewVisitorTest(unittest.TestCase):
         # Ela percebe que p título da página e o cabeçalho mencionam listas de
         # tarefas (to-do)
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-        # Ela é convidada a inserri um item de tarefa imediatamente 
+        # Ela é convidada a inserri um item de tarefa imediatamente
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # Ela digita "Buy peacock feathers" (Comprar penas de pavão) em uma caixa
         # de texto (o hoby de Gabriel é fazer iscas para pescas com fly)
+        inputbox.send_keys('Buy peacock feathers')
 
         # Quando ela tecla enter, a página é atualizada, e agora a página lista
         # "1: Buy peacock feather" como um item em uma lista de tarefas
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows )
+        )
 
         # Ainda continua havendo uma caixa de texto convidando-a a acrecentar outro
         # item. Ela insere "Use peacock feathers to make a fly" (Usar penas de pavão
         # para fazer um fly - Edith é bem metódica)
+        self.fail('Finish the test!')
 
         # A página é atualizada novamente e agora mostras os dois itens em sua lista
 
