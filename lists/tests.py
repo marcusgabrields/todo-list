0e267c93d3dd395e7650704538dd8ae3e8744150
Marcus Gabriel Dantas Salgues <marcusgabriel.ds@gmail.com>
2018-08-31 23:51:26 -0300

    Refactor home page view to use template and corrected the syntax off FT

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from .views import home_page
 
@@ -11,10 +12,6 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-    def test_home_page_return_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title> To-Do lists </title>', html)
-        self.assertTrue(html.endswith('</html>'))
+    def test_user_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
