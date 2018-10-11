cdeeda84f9da32a277c6130d79d9104a180fe4f8
Marcus Gabriel Dantas Salgues <marcusgabriel.ds@gmail.com>
2018-10-11 19:37:42 -0300

    Add a new test

--- lists/tests.py
+++ lists/tests.py
@@ -64,3 +64,16 @@ class ListViewTest(TestCase):
 
         self.assertContains(response, 'itemy 1')
         self.assertContains(response, 'itemy 2')
+
+
+class NewListTest(TestCase):
+
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.frist()
+        self.assertEqual(new_item, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
